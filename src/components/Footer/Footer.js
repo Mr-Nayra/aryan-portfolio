178e19875f4181df8073c220ff985128b89f87fd
Mr.Nayra <aryanedu5786@gmail.com>
2023-09-23 15:27:29 +0530

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = (props) => {
   return (
-    <div className="lg:hidden fixed bottom-0 right-0 w-full flex bg-[#343534] border border-zin-600 py-4 rounded-t-2xl items-center justify-center opacity-90">
+    <div className="lg:hidden fixed bottom-0 right-0 w-full flex bg-[#343534] border border-zinc-600 py-4 rounded-t-2xl items-center justify-center opacity-90">
       <div className="flex gap-5">
         <Link href="/">
           <p className={`${props.heading == "About me" && "text-[#FDCE69]"}`}>
